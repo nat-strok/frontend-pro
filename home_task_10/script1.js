c2713d9b003ddccd72fbc39f13c14c53db269f5b
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-07-14 09:28:37 +0300

    made ht 10

---
+++ home_task_10/script1.js
@@ -0,0 +1,50 @@
+function taskOne(oldText, newText) {
+    const allElements = document.body.querySelectorAll('*');
+    let currentElement;
+    for (let elem of allElements) {
+        if (elem.textContent === oldText) {
+            currentElement = elem;
+            return currentElement.textContent = newText;
+        }
+    }
+}
+
+function taskTwo() {
+    const elem = document.querySelector('[my-attribute]');
+    elem.removeAttribute('my-attribute');
+}
+
+function taskThree(selector, color) {
+    const allElements = document.querySelectorAll(selector);
+    for (let elem of allElements) {
+        elem.style.backgroundColor = color;
+    }
+}
+
+function taskFour(tdOneValue, tdTwoValue, tdThreeValue) {
+
+    function newElement(tag, content) {
+        let elem = document.createElement(tag);
+        elem.textContent = content;
+        return elem;
+    }
+
+    const parentElem = document.querySelector('tbody');
+    const previousElem = document.querySelector('table tr:last-child');
+    const newRow = parentElem.insertBefore(newElement('tr'), previousElem);
+    newRow.appendChild(newElement('td', tdOneValue));
+    newRow.appendChild(newElement('td', tdTwoValue));
+    newRow.appendChild(newElement('td', tdThreeValue));
+}
+
+function taskFive() {
+    const parentElem = document.querySelector('tbody');
+    const firstRow = document.querySelector('table th').parentElement.nextElementSibling;
+    parentElem.removeChild(firstRow);
+}
+
+taskOne('2021', '2020');
+taskTwo();
+taskThree('[data-id]', 'rgba(49,123,24,0.25)');
+taskFour('Any Company', 'Any Contact', 0);
+taskFive();
