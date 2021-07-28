e0f2a417a2d0956087f8ed8a4fef1429c3b4afbb
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-07-28 23:00:55 +0300

    ht_14 done

---
+++ home_task_14/script1-2.js
@@ -0,0 +1,24 @@
+function arrayFill(item, length) {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(item);
+    }
+    return arr;
+}
+
+console.log(arrayFill('x', 5));
+
+
+function arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j];
+        }
+    }
+    return sum;
+}
+
+let array = [[1, 2, 3], [4, 5], [6]];
+console.log(arraySum(array));
+console.log(array.flat(Infinity).reduce((sum, item) => sum += item));
