0bf2f8d787ed21b643de9cb1749e9498d4c92340
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-08-13 10:18:19 +0300

    ht_19 redone with Date.prototype extension

--- home_task_19/script.js
+++ home_task_19/script.js
@@ -1,21 +1,20 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function getWeek(now, start) {
-    let firstWeekPrevDays = start.getDay();
-    let daysDiff = (now.getTime() - start.getTime()) / (1000 * 60 * 60 * 24);
+Date.prototype.getWeek = function () {
+    const startYear = new Date(this.getFullYear(), 0, 1);
+    let firstWeekPrevDays = startYear.getDay();
+    let daysDiff = (this.getTime() - startYear.getTime()) / (1000 * 60 * 60 * 24);
     return Math.ceil((daysDiff + firstWeekPrevDays) / 7);
 }
 
 function printTime() {
     const date = new Date();
-    const startYear = new Date(date.getFullYear(), 0, 1);
-
     document.body.querySelector('[data-id="hours"]').textContent = (date.getHours() < 10) ? `0${date.getHours()}` : date.getHours();
     document.body.querySelector('[data-id="min"]').textContent = ('0' + date.getMinutes()).slice(-2);
     document.body.querySelector('[data-id="sec"]').textContent = ('0' + date.getSeconds()).slice(-2);
     document.body.querySelector('[data-id="date"]').textContent = `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-    document.body.querySelector('[data-id="week"]').textContent = `The ${getWeek(date, startYear)}th week`;
+    document.body.querySelector('[data-id="week"]').textContent = `The ${date.getWeek()}th week`;
 }
 
 function updateClock() {
