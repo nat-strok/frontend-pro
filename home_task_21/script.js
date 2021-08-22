1b3c8ea47bbd6e6a670b03b840272613ee06c5e1
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-08-23 00:22:42 +0300

    ht_21 done

--- home_task_21/script.js
+++ home_task_21/script.js
@@ -1,36 +1,31 @@
-let itemTemplate = document.querySelector('#itemTemplate').innerHTML;
-const listToDo = document.querySelector('#listToDo');
+document.addEventListener('DOMContentLoaded', () => {
+    let itemTemplate = document.querySelector('#itemTemplate').innerHTML;
+    const listToDo = document.querySelector('#listToDo');
+    const loadToDos = document.querySelector('#loadToDos');
+    loadToDos.addEventListener('click', getListToDo);
 
-document.addEventListener("DOMContentLoaded", () => {
-    getListToDo();
-    listToDo.addEventListener('click', onClickChangeItem);
+    function getListToDo() {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((res) => res.json())
+            .then((data) => handleToDo(data))
+            .catch((error) => console.log(error.message))
+            .finally(() => loadToDos.remove());
+    }
+
+    function handleToDo(data) {
+        data.forEach(item => addItem(item));
+        listToDo.addEventListener('click', onClickChangeItem);
+    }
+
+    function addItem(item) {
+        let html = itemTemplate.replace('{{title}}', item.title);
+        html = html.replace('{{id}}', item.id);
+        html = !!item['completed'] ? html.replace('{{completed}}', ' done') : html.replace('{{completed}}', '');
+        listToDo.insertAdjacentHTML('beforeend', html);
+    }
+
+    function onClickChangeItem(event) {
+        if (event.target.closest('li')) event.target.classList.toggle('done');
+        if (event.target.classList.contains('close')) event.target.parentElement.remove();
+    }
 });
-
-function getListToDo() {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((res) => res.json())
-        .then((data) => handleToDo(data))
-        .catch((error) => console.log(error.message));
-}
-
-function handleToDo(data) {
-    data.forEach(item => addItem(item));
-}
-
-function addItem(item) {
-    let html = itemTemplate.replace('{{title}}', item.title);
-    html = html.replace('{{id}}', item.id);
-    item.completed === true ? html = html.replace('{{completed}}', 'done') : html = html.replace('{{completed}}', 'whatToDo');
-    listToDo.insertAdjacentHTML('beforeend', html);
-}
-
-function onClickChangeItem(event) {
-    if (event.target.closest('li')) {
-        event.target.classList.toggle('done');
-        event.target.classList.toggle('whatToDo');
-    }
-    if (event.target.classList.contains('close')) {
-        event.target.parentElement.remove();
-    }
-}
-
