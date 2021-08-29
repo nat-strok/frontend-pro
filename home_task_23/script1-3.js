a7fa0a3a1ae10ee74e5893a17d86b9ddcf282cb7
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-08-29 17:01:30 +0300

    ht_23 done

---
+++ home_task_23/script1-3.js
@@ -0,0 +1,14 @@
+// task 1
+const countTotal = () => ({total: 100});
+console.log(countTotal());
+
+// task 2
+const result = `Today is ${new Date}`
+console.log(result);
+
+// task 3
+let a = 2;
+let b = 3;
+console.log(`Initial values: a=${a}, b=${b}`);
+[a, b] = [b, a];
+console.log(`New values: a=${a}, b=${b}`);
