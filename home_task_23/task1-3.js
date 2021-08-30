da588eb2f997b95a38c01c2a983125b9c6ad8c51
nat-strok <86675365+nat-strok@users.noreply.github.com>
2021-08-30 22:09:56 +0300

    ht_23 done

--- home_task_23/task1-3.js
+++ home_task_23/task1-3.js
@@ -7,8 +7,8 @@ const result = `Today is ${new Date}`
 console.log(result);
 
 // task 3
-let a = 2;
-let b = 3;
+let a = 2,
+    b = 3;
 console.log(`Initial values: a=${a}, b=${b}`);
 [a, b] = [b, a];
 console.log(`New values: a=${a}, b=${b}`);
